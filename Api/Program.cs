28f810acc1f5e1a3436fb864a21fcf77c98ec6ca
Thomas Hallström <minimoto@hotmail.com>
2025-05-16 18:54:06 +0200

    change in program

--- Api/Program.cs
+++ Api/Program.cs
@@ -8,7 +8,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<CleaningSaboms.Context.DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure())
+    );
 
 
 var app = builder.Build();
