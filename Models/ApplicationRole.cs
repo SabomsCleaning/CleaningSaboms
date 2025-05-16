583675158cbd98f7ea2c12bb406c2be66fc8adfc
Thomas Hallström <minimoto@hotmail.com>
2025-05-16 16:46:37 +0200

    update in datacontext

---
+++ Models/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleaningSaboms.Models
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(30)]
+        public string RoleName { get; set; }
+
+        public ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+    }
+}
