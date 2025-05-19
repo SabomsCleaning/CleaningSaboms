184e2a21a77a7ab631007a5ff0d3656be25dc8f5
Thomas Hallström <minimoto@hotmail.com>
2025-05-19 22:14:33 +0200

    Add tests for createCustomer

--- Api/Models/CustomerAddressEntity.cs
+++ Api/Models/CustomerAddressEntity.cs
@@ -1,11 +1,25 @@
-﻿namespace CleaningSaboms.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleaningSaboms.Models
 {
     public class CustomerAddressEntity
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string CustomerAddressLine { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
         public string CustomerCity { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
         public string CustomerPostalCode { get; set; } = null!;
+
+        [Required]
         public Guid CustomerId { get; set; }
     }
 }
